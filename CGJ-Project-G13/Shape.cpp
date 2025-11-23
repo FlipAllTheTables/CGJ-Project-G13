fdbcf87bdba1a585d8a337fec22effb176f8f56c
FlipAllTheTables <rafaelbgwr@gmail.com>
2025-11-23 15:57:32 +0000

    Inverted order of operations inside transformation methods

--- CGJ-Project-G13/Shape.cpp
+++ CGJ-Project-G13/Shape.cpp
@@ -9,16 +9,16 @@ Shape::Shape(GLuint VAO, glm::vec3 color, GLint MatrixId, GLint ColorId, glm::ma
 }
 
 void Shape::translate(const glm::vec3& delta) {
-	this->transform = glm::translate(this->transform, delta);
+	this->transform = glm::translate(glm::mat4(1.0f), delta) * this->transform;
 }
 
 void Shape::scale(const glm::vec3& scale) {
-	this->transform = glm::scale(this->transform, scale);
+	this->transform = glm::scale(glm::mat4(1.0f), scale) * this->transform;
 }
 
 // TODO: Implement using quaternions?
 void Shape::rotate(const glm::vec3& axis, float angle) {
 	float rad = glm::radians(angle);
 	glm::vec3 normAxis = glm::normalize(axis);
-	this->transform = glm::rotate(this->transform, rad, normAxis);
+	this->transform = glm::rotate(glm::mat4(1.0f), rad, normAxis) * this->transform;
 }
